1d51dc29ae491270cf3859e80145abaad3809ce8
hillwill <2537660666@qq.com>
2024-03-28 08:00:10 +0800

    加入自动发布gh-pages

---
+++ gh-pages.js
@@ -0,0 +1,7 @@
+var ghpages = require('gh-pages');
+ghpages.publish('dist', {
+    branch: 'gh-pages',
+    repo: 'https://github.com/wizount/element-plus-designer.git'
+}, function (err) {
+    console.info(err)
+});
