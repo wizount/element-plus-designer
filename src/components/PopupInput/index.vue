03669da08b9c88fb1e46de5b34fce60d73c1db98
hillwill <2537660666@qq.com>
2024-05-14 08:54:45 +0800

    将popupInput变成Promise

--- src/components/PopupInput/index.vue
+++ src/components/PopupInput/index.vue
@@ -3,6 +3,11 @@
     <div ref="popupRef"
          style="position: fixed; background-color: white;padding: 15px;border: 1px #c6c6c8 solid; border-radius: 5px;">
       <slot></slot>
+
+      <div style="text-align: right">
+        <el-button @click="cancel">取消</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
+      </div>
     </div>
   </div>
 
@@ -12,30 +17,65 @@
 
 const popupRef = ref();
 const overlay = ref();
-const popup = (pos) => {
-  const el = popupRef.value;
-  overlay.value.style.display = 'block'
-  nextTick(() => {
-    if (pos.x + el.clientWidth > window.innerWidth) {
-      el.style.left = (pos.x - el.clientWidth - 10) + 'px'
-    } else {
-      el.style.left = (pos.x + 10) + 'px'
-    }
-    if (pos.y + el.clientHeight > window.innerHeight) {
-      el.style.top = Math.max(0, (pos.y - el.clientHeight - 10)) + 'px'
-    } else {
-      el.style.top = (pos.y + 10) + 'px'
-    }
-  })
+const messageInstance = {
+  value: null,
+  resolve: null,
+  reject: null
 }
+const popup = (pos, value) => {
+  messageInstance.value = value;
+  return new Promise((resolve, reject) => {
+    messageInstance.resolve = resolve;
+    messageInstance.reject = reject;
+    const el = popupRef.value;
+    overlay.value.style.display = 'block'
+    nextTick(() => {
+      let left
+      if (pos.x + el.clientWidth > window.innerWidth) {
+        left = (pos.x - el.clientWidth) + 'px'
+      } else {
+        left = (pos.x) + 'px'
+      }
+      if (left < 0) {
+        left = 0;
+      }
+      el.style.left = left;
+      let top
+      if (pos.y + el.clientHeight > window.innerHeight) {
+        top = (pos.y - el.clientHeight) + 'px'
+      } else {
+        top = (pos.y) + 'px'
+      }
+      if (top < 0) {
+        top = 0;
+      }
+      el.style.top = top;
+    })
+  })
+
+}
+
+function confirm() {
+  overlay.value.style.display = 'none';
+  if (messageInstance.value && messageInstance.resolve) {
+    messageInstance.resolve(messageInstance.value);
+  }
+}
+
+const cancel = () => {
+  overlay.value.style.display = 'none';
+  if (messageInstance.value && messageInstance.reject) {
+    messageInstance.reject("cancel");
+  }
+}
+
 const close = (event) => {
   if (!event || event.target === overlay.value) {
     overlay.value.style.display = 'none'
   }
 }
-
 defineExpose({
-  popup, close
+  popup
 });
 </script>
 
